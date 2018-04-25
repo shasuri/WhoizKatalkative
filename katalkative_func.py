2deac2eb98c49a6445a0f7221fdc6563dc329b03
sha <ghimmk@gmail.com>
2018-04-26 03:44:40 +0900

    modifying chatLineChecker
    structuring chatLine by each type

--- katalkative_func.py
+++ katalkative_func.py
@@ -4,18 +4,41 @@ import datetime
 class chatMember:
 	def __init__(self, name, invitedDate):
 		self.name = name
-		self.invitedDate = invitedDate
-		self.chattingSize = 0
+		self.invitedDate = invitedDate		
 		self.chatCount = 0
+		
+		self.talkSize = 0
+		self.talkCount = 0
+		
+		self.emoticonCount = 0
+		self.imageCount = 0
+		self.linkCount = 0
+		self.videoCount = 0
+		self.shareAddressCounter = 0
+		self.hashtagCounter = 0
 	
-	def printName(self):
-		print self.name
-	
-	def chattingSizeAdder(self, chattingLength):
-		self.chattingSize += chattingLength
+	def talkSizeAdder(self, chattingLength):
+		self.talkSize += chattingLength
 	
 	def chatCounter(self):
 		self.chatCount += 1
+
+	def talkCounter(self):
+		self.talkCount += 1
+		chatCounter()
+	
+	def imageCounter(self):
+		self.imageCount += 1
+		chatCounter()
+	def shareAddressCounter(self):
+		self.shareAddressCount += 1
+		chatCounter()
+	def videoCounter(self):
+		self.videoCount += 1
+		chatCounter()
+	def hashtagCounter(self):
+		self.hashtagCount += 1
+		chatCounter()
 
 def chatFileOpener(fileName):
 	file = open(fileName, 'r')
@@ -61,14 +84,17 @@ def blankLinePasser(passFile,lineNum):
 
 def lineTypeChecker(chatSingleLine, memberList):
 	
+	dividerPos = chatSingleLine.find(', ')
+	dateLine = chatDateChecker(chatSingleLine[:dividerPos])
+	chatterContentLine = chatSingleLine[dividerPos+2:]
+
 	if(extraLineChecker(chatSingleLine)):
-		if(chatSingleLine.find(' : ') > 0):
+		if(chatterContentLine.find(' : ') > -1):
 			lineType = 'chat'
 
-		elif(chatSingleLine.find('초대했습니다.') > 0):
+		elif(chatterContentLine.find('초대했습니다.') > -1):
 			lineType = 'invite'
-			inviteLineChecker(chatSingleLine, memberList)
-		
+			inviteLineChecker(chatSingleLine, memberList, dateLine)
 		else:
 			lineType = 'date'
 
@@ -94,14 +120,15 @@ def extraLineChecker(chatSingleLine):
 
 	return extraLineCheck
 
-def inviteLineChecker(inviteLine, memberList):
+def inviteLineChecker(inviteLine, memberList, inviteDate):
 	
-	dividerPos = inviteLine.find(', ')
+	#dividerPos = inviteLine.find(', ')
 
-	inviteDateLine = inviteLine[:dividerPos]
-	inviteDate = chatDateChecker(inviteDateLine)
+	#inviteDateLine = inviteLine[:dividerPos]
+	#inviteDate = chatDateChecker(inviteDateLine)
 	
-	inviteMemberData = inviteLine[dividerPos+2:]
+	#inviteMemberData = inviteLine[dividerPos+2:]
+	inviteMemberData = inviteLine
 	inviteMemberData = inviteMemberData.split('님')
 	inviteMemberData.pop()
 	
@@ -125,20 +152,58 @@ def chatterSearcher(memberList, searchMemberName):
 
 	return targetInstance
 
-def chatLineChecker(chatLine, memberList):
-	decodeType = 'utf8'
+def talkLineChecker(talkLine, talker):
+	'''
+	emoticonCheck = chatLine.find('(이모티콘)')
+	imageCheck = chatLine.find('<사진>')
+	if emoticonCheck > 0:
+		if chatLine[emoticonCheck:] != '\n':
+			talkLineChecker
+	'''
+	#(이모티콘)
+	#<사진>
+	#http:// or https://
+	#<동영상>
 
-	dateDividerPos = chatLine.find(', ')
-	
-	chatLine = chatLine[dateDividerPos+2:]
+
+def chatLineChecker(chatLine, memberList):
+	#decodeType = 'utf8'
+	#dateDividerPos = talkLine.find(', ')
+	#chatLine = chatLine[dateDividerPos+2:]
 
 	chattingDividerPos = chatLine.find(' : ')
 	
 	chatter = chatLine[:chattingDividerPos]
 	chatting = chatLine[chattingDividerPos+3:]
+	
+	emoticonCheck = chatting.find('(이모티콘)')
+	'''
+	if(emoticonCheck > -1):
+		chatter.emoticonCounter()
+		talkWithEmoticon = chatLine[emoticonCheck+14:]
+		
+		if talkWithEmoticon != '\n':
+			talkLineChecker(talkWithEmoticon, chatter)
+	'''
+	if(chatting == '<사진>\n'):
+		chatter.imageCounter()
+	
+	elif(chatting == '<동영상>\n'):
+		chatter.videoCounter()
+	elif(chatting.find('#') == 0)
+		chatter.hashtagCounter()
+	elif(chatting.find('<연락처') == 0):
+		chatter.shareAddressCounter()
+	else:
+		talkLineChecker(chatting, chatter)
+	#elif(chatting.find('http://') > -1 or chatting.find('https://') > -1):
+	#	chatter.linkCounter
 
+	
+	'''
 	chatterInstance = chatterSearcher(memberList, chatter)
 	chattingLength = len(chatting.decode(decodeType)) - 1
 
 	chatterInstance.chattingSizeAdder(chattingLength)
 	chatterInstance.chatCounter()
+	'''
