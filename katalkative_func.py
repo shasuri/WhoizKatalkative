d5c0788209ad941c4153ff75e8db24dffbe9e0ba
sha <ghimmk@gmail.com>
2018-04-29 06:52:48 +0900

    completed prototype
    it can print on console

--- katalkative_func.py
+++ katalkative_func.py
@@ -63,9 +63,14 @@ def printMemberInfo(memberList):
 		print (singleInstance.name).decode(decodeType) + "'s Information : "
 		print "\tInvited date : " + str(singleInstance.invitedDate)
 		print "\tchatted %d times" % singleInstance.chatCount
-		print "\ttalked  %d times" % singleInstance.talkCount
+		print "\ttalked %d times" % singleInstance.talkCount
 		print "\ttalked %d letters" % singleInstance.talkSize
 		print "\tlinked %d pages" % singleInstance.linkCount
+		print "\tused %d emoticons" % singleInstance.emoticonCount
+		print "\tuploaded %d images" % singleInstance.imageCount
+		print "\ttaged %d hashtags" % singleInstance.hashtagCount
+		print "\tuploded %d videos" % singleInstance.videoCount
+		print "\tuploded %d files(except image and video)" % singleInstance.fileCount
 
 def chatFileOpener(fileName):
 	file = open(fileName, 'r')
@@ -142,6 +147,7 @@ def lineTypeChecker(chatSingleLine, memberList, recentChatter):
 		return presentChatter
 	else:
 		lineType = 'blank'
+		return recentChatter
 
 def mainLineChecker(chatSingleLine):
 	yearPos = chatSingleLine.find('년')
@@ -197,9 +203,26 @@ def linkChecker(linkChat, linkUploader, linkPos):
 	
 	beforeLink = linkChat[:linkPos]
 	
-	linkChat = beforeLink + afterLink
+	afterLinkPos = linkPosFinder(afterLink)
 	
-	return linkChat
+	if(afterLinkPos > -1):
+		afterLink = linkChecker(afterLink, linkUploader, afterLinkPos)
+	
+	exceptChat = beforeLink + afterLink
+	
+	return exceptChat
+
+def linkPosFinder(assumptLinkChat):
+	httpPos = assumptLinkChat.find('http://')
+	httpsPos = assumptLinkChat.find('https://')
+	linkPos = -1
+	
+	if(httpPos > -1):
+		linkPos = httpPos
+	elif(httpsPos > -1):
+		linkPos = httpsPos
+
+	return linkPos
 
 def talkLineChecker(talkLine, talker):
 	decodeType = 'utf-8'
@@ -207,30 +230,34 @@ def talkLineChecker(talkLine, talker):
 	if(talkLine.find('(이모티콘)') == 0):
 		talker.emoticonCounter()
 		talkLine = talkLine[14:]
-	
-	httpPos = talkLine.find('http://')
-	httpsPos = talkLine.find('https://')
-	linkPos = -1
-	if(httpPos > -1):
-		linkPos = httpPos
-	elif(httpsPos > -1):
-		linkPos = httpsPos
 
-	if(linkPos > -1):
+	linkPos = linkPosFinder(talkLine)
+	
+	if linkPos > -1:
 		talkLine = linkChecker(talkLine, talker, linkPos)
+
 	if(talkLine != '\n'):
-		talkLength = len(talkLine.decode(decodeType))-1
+		try:
+			talkLength = len(talkLine.decode(decodeType))-1
+		except:
+			talkLength = len(talkLine)/3
 		talker.talkCounter(talkLength)
 	return talker
 	
 def fileChecker(fileLine, fileUploader, dotPos):
 	supportFileList = ['mp4', 'm4v', 'avi', 'asf', 'wmv', 'mkv', 'ts', 'mpg', 'mpeg', 'mov', 'flv', 'ogv', 'doc', 'docx', 'hwp', 'txt', 'rtf', 'xml', 'pdf', 'wks', 'wps', 'xps', 'md', 'odf', 'odt', 'ods', 'odp', 'csv', 'tsv', 'xls', 'xlsx', 'ppt', 'pptx', 'pages', 'key', 'numbers', 'show', 'ce', 'zip', 'gz', 'bz2', 'rar', '7z', 'lzh', 'alz']
 	
-	assumptExtension = fileLine[dotPos+1:]
+	assumptExtension = fileLine[dotPos+1:-1]
+
+	fileExist = False
 	
 	for fileExtension in supportFileList:
 		if (assumptExtension == fileExtension):
 			fileUploader.fileCounter()
+			fileExist = True
+			break
+
+	return fileExist
 
 def chatLineChecker(chatLine, memberList):
 
@@ -258,10 +285,17 @@ def chatLineChecker(chatLine, memberList):
 	else:
 		dotPos = chatting.find('.')
 		chattingSize = len(chatting)
-		
-		if(dotPos == chattingSize - 4 or dotPos == chattingSize - 5 or dotPos == chattingSize - 3):
-			fileChecker(chatting, chatterInstance, dotPos)
+		fileExist = False
+		#print chatting.decode('utf-8')
+		#print chatting[dotPos:].decode('utf-8')
+		#print chattingSize - dotPos
+
+		if chattingSize > 6 and ((dotPos == chattingSize - 4) or (dotPos == chattingSize - 5) or (dotPos == chattingSize - 6)):
+			#print 'Assumpt file',
 			
-		else:
+			fileExist = fileChecker(chatting, chatterInstance, dotPos)
+		
+		if not fileExist:
 			talkLineChecker(chatting, chatterInstance)
 	return chatterInstance
+	
--- katalkative_main.py
+++ katalkative_main.py
@@ -2,7 +2,7 @@
 from katalkative_func import *
 
 decodeType = 'utf8'
-logFileName = "../KakaoTalkChats.txt"
+logFileName = "../Katalkative_texts/KakaoTalkChats_bigfile.txt"
 logFile = chatFileOpener(logFileName)
 
 logFilePresentLine = next(logFile)
@@ -16,25 +16,15 @@ logSavedDate = chatDateChecker(slicedDateLine)
 
 blankLinePasser(logFile, 2)
 
-#i = 5
+i = 5
 
 chatMemberList = []
 previousChatter = 'Blank'
 for logFilePresentLine in logFile:
-		previousChatter = lineTypeChecker(logFilePresentLine, chatMemberList, previousChatter)
-'''
-	try:
-		lineType = lineTypeChecker(logFilePresentLine, chatMemberList)
+	previousChatter = lineTypeChecker(logFilePresentLine, chatMemberList, previousChatter)
+	#print i
+	#i += 1
 
-	except:
-		print 'errored line',
-		print logFilePresentLine.decode(decodeType)
-'''
-'''
-	print i, 
-	print logFilePresentLine.decode(decodeType)
-	i += 1
-'''
 print "Chatting Title : " + chatTitle
 print "Chatting Member Number : %d" % chatMemberNum
 print "Log is saved at " + str(logSavedDate)
