b2c40998aa70b63293a8fe9cad069f2fec5294f7
sha <ghimmk@gmail.com>
2018-05-02 17:17:37 +0900

    add the py3version
    add py3 version and start work

---
+++ Py3ver/katalkative_main_Py3.py
@@ -0,0 +1,23 @@
+#-*- coding: utf-8 -*-
+from katalkative_func import *
+
+decodeType = 'utf-8'
+logFileName = "../Katalkative_texts/KakaoTalkChats_bigfile.txt"
+logFile = chatFileOpener(logFileName)
+
+logFilePresentLine = next(logFile)
+chatRoomInstance = chatRoomInfoChecker(logFilePresentLine)
+
+logFilePresentLine = next(logFile)
+chatRoomInstance.setLogSaveDate(chatDateChecker(logFilePresentLine[19:]))
+
+blankLinePasser(logFile, 2)
+
+i = 5
+
+for logFilePresentLine in logFile:
+	lineTypeChecker(logFilePresentLine, chatRoomInstance)
+	#print i
+	i += 1
+printChatInfo(chatRoomInstance)
+logFile.close()
